fc2af2936c81a5e000876c70e6bf24c0164dc2df
Edson Moura <edson@lovetatoo.com.br>
2019-03-16 14:24:38 -0300

    WTest app started

---
+++ WTest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WTest
+//
+//  Created by Edson iMAC on 16/03/2019.
+//  Copyright © 2019 Edson Moura. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
